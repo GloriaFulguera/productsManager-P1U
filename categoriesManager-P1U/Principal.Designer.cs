2ca16864981be391a44b0f7845b62b9d13d05c0a
Gloria Fulguera <gloriafulguera@gmail.com>
2023-06-04 18:11:50 -0300

    Funcionalidad boton Agregar, Cancelar y Agregar

---
+++ categoriesManager-P1U/Principal.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace categoriesManager_P1U
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Principal));
+            this.grbManager = new System.Windows.Forms.GroupBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.txbDescripcion = new System.Windows.Forms.TextBox();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.txbCodigo = new System.Windows.Forms.TextBox();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.grbBotones = new System.Windows.Forms.GroupBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnReporte = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.lsbInformacion = new System.Windows.Forms.ListBox();
+            this.lblMensajes = new System.Windows.Forms.Label();
+            this.grbManager.SuspendLayout();
+            this.grbBotones.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grbManager
+            // 
+            this.grbManager.Controls.Add(this.btnAceptar);
+            this.grbManager.Controls.Add(this.btnCancelar);
+            this.grbManager.Controls.Add(this.txbDescripcion);
+            this.grbManager.Controls.Add(this.lblDescripcion);
+            this.grbManager.Controls.Add(this.txbCodigo);
+            this.grbManager.Controls.Add(this.lblCodigo);
+            this.grbManager.Enabled = false;
+            this.grbManager.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.grbManager.Location = new System.Drawing.Point(12, 12);
+            this.grbManager.Name = "grbManager";
+            this.grbManager.Size = new System.Drawing.Size(411, 257);
+            this.grbManager.TabIndex = 11;
+            this.grbManager.TabStop = false;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.BackColor = System.Drawing.Color.White;
+            this.btnAceptar.FlatAppearance.BorderSize = 0;
+            this.btnAceptar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAceptar.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.Location = new System.Drawing.Point(296, 191);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(97, 30);
+            this.btnAceptar.TabIndex = 16;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = false;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.White;
+            this.btnCancelar.FlatAppearance.BorderSize = 0;
+            this.btnCancelar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(174, 191);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(97, 30);
+            this.btnCancelar.TabIndex = 15;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // txbDescripcion
+            // 
+            this.txbDescripcion.Font = new System.Drawing.Font("Bahnschrift", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbDescripcion.Location = new System.Drawing.Point(174, 88);
+            this.txbDescripcion.MaxLength = 60;
+            this.txbDescripcion.Multiline = true;
+            this.txbDescripcion.Name = "txbDescripcion";
+            this.txbDescripcion.Size = new System.Drawing.Size(219, 77);
+            this.txbDescripcion.TabIndex = 14;
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Font = new System.Drawing.Font("Bahnschrift", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescripcion.Location = new System.Drawing.Point(40, 91);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(128, 23);
+            this.lblDescripcion.TabIndex = 13;
+            this.lblDescripcion.Text = "DESCRIPCION";
+            // 
+            // txbCodigo
+            // 
+            this.txbCodigo.Font = new System.Drawing.Font("Bahnschrift", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbCodigo.Location = new System.Drawing.Point(174, 36);
+            this.txbCodigo.MaxLength = 3;
+            this.txbCodigo.Name = "txbCodigo";
+            this.txbCodigo.Size = new System.Drawing.Size(164, 30);
+            this.txbCodigo.TabIndex = 12;
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Font = new System.Drawing.Font("Bahnschrift", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigo.Location = new System.Drawing.Point(40, 39);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(75, 23);
+            this.lblCodigo.TabIndex = 11;
+            this.lblCodigo.Text = "CODIGO";
+            // 
+            // grbBotones
+            // 
+            this.grbBotones.Controls.Add(this.btnSalir);
+            this.grbBotones.Controls.Add(this.btnReporte);
+            this.grbBotones.Controls.Add(this.btnEliminar);
+            this.grbBotones.Controls.Add(this.btnActualizar);
+            this.grbBotones.Controls.Add(this.btnNuevo);
+            this.grbBotones.Location = new System.Drawing.Point(12, 321);
+            this.grbBotones.Name = "grbBotones";
+            this.grbBotones.Size = new System.Drawing.Size(776, 117);
+            this.grbBotones.TabIndex = 12;
+            this.grbBotones.TabStop = false;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.White;
+            this.btnSalir.FlatAppearance.BorderSize = 0;
+            this.btnSalir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCoral;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Font = new System.Drawing.Font("Bahnschrift", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Image = ((System.Drawing.Image)(resources.GetObject("btnSalir.Image")));
+            this.btnSalir.Location = new System.Drawing.Point(665, -2);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(105, 105);
+            this.btnSalir.TabIndex = 9;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnSalir.UseVisualStyleBackColor = false;
+            // 
+            // btnReporte
+            // 
+            this.btnReporte.BackColor = System.Drawing.Color.White;
+            this.btnReporte.FlatAppearance.BorderSize = 0;
+            this.btnReporte.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkTurquoise;
+            this.btnReporte.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReporte.Font = new System.Drawing.Font("Bahnschrift", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReporte.Image = ((System.Drawing.Image)(resources.GetObject("btnReporte.Image")));
+            this.btnReporte.Location = new System.Drawing.Point(426, -2);
+            this.btnReporte.Name = "btnReporte";
+            this.btnReporte.Size = new System.Drawing.Size(105, 105);
+            this.btnReporte.TabIndex = 8;
+            this.btnReporte.Text = "REPORTE";
+            this.btnReporte.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnReporte.UseVisualStyleBackColor = false;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.White;
+            this.btnEliminar.FlatAppearance.BorderSize = 0;
+            this.btnEliminar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkTurquoise;
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.Font = new System.Drawing.Font("Bahnschrift", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Image = ((System.Drawing.Image)(resources.GetObject("btnEliminar.Image")));
+            this.btnEliminar.Location = new System.Drawing.Point(288, 0);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(105, 105);
+            this.btnEliminar.TabIndex = 7;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.BackColor = System.Drawing.Color.White;
+            this.btnActualizar.FlatAppearance.BorderSize = 0;
+            this.btnActualizar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkTurquoise;
+            this.btnActualizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnActualizar.Font = new System.Drawing.Font("Bahnschrift", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnActualizar.Image = ((System.Drawing.Image)(resources.GetObject("btnActualizar.Image")));
+            this.btnActualizar.Location = new System.Drawing.Point(143, -2);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(105, 105);
+            this.btnActualizar.TabIndex = 6;
+            this.btnActualizar.Text = "ACTUALIZAR";
+            this.btnActualizar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnActualizar.UseVisualStyleBackColor = false;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.BackColor = System.Drawing.Color.White;
+            this.btnNuevo.FlatAppearance.BorderSize = 0;
+            this.btnNuevo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkTurquoise;
+            this.btnNuevo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNuevo.Font = new System.Drawing.Font("Bahnschrift", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNuevo.Image = ((System.Drawing.Image)(resources.GetObject("btnNuevo.Image")));
+            this.btnNuevo.Location = new System.Drawing.Point(0, -2);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(105, 105);
+            this.btnNuevo.TabIndex = 5;
+            this.btnNuevo.Text = "NUEVO";
+            this.btnNuevo.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnNuevo.UseVisualStyleBackColor = false;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // lsbInformacion
+            // 
+            this.lsbInformacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lsbInformacion.FormattingEnabled = true;
+            this.lsbInformacion.ItemHeight = 16;
+            this.lsbInformacion.Location = new System.Drawing.Point(454, 19);
+            this.lsbInformacion.Name = "lsbInformacion";
+            this.lsbInformacion.Size = new System.Drawing.Size(333, 212);
+            this.lsbInformacion.TabIndex = 13;
+            // 
+            // lblMensajes
+            // 
+            this.lblMensajes.AutoSize = true;
+            this.lblMensajes.BackColor = System.Drawing.Color.White;
+            this.lblMensajes.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensajes.ForeColor = System.Drawing.Color.Maroon;
+            this.lblMensajes.Location = new System.Drawing.Point(451, 244);
+            this.lblMensajes.Margin = new System.Windows.Forms.Padding(0);
+            this.lblMensajes.Name = "lblMensajes";
+            this.lblMensajes.Padding = new System.Windows.Forms.Padding(2);
+            this.lblMensajes.Size = new System.Drawing.Size(4, 19);
+            this.lblMensajes.TabIndex = 14;
+            this.lblMensajes.Visible = false;
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMensajes);
+            this.Controls.Add(this.lsbInformacion);
+            this.Controls.Add(this.grbBotones);
+            this.Controls.Add(this.grbManager);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Principal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PRODUCTS MANAGER";
+            this.grbManager.ResumeLayout(false);
+            this.grbManager.PerformLayout();
+            this.grbBotones.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox grbManager;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.TextBox txbDescripcion;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.TextBox txbCodigo;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.GroupBox grbBotones;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnReporte;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.ListBox lsbInformacion;
+        private System.Windows.Forms.Label lblMensajes;
+    }
+}
---
+++ categoriesManager-P1U/Principal.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace categoriesManager_P1U
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            grbBotones.Enabled= false;
+            grbManager.Enabled = true;
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            grbBotones.Enabled = true;
+            grbManager.Enabled = false;
+            txbCodigo.Text = "";
+            txbDescripcion.Text = "";
+
+            lblMensajes.Text = "";
+            lblMensajes.Visible= false;
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            string item = "";
+            if (txbCodigo.Text != "" && txbDescripcion.Text != "")
+            {
+                item += txbCodigo.Text + "    " + txbDescripcion.Text;
+                lsbInformacion.Items.Add(item);
+                lblMensajes.Text = "";
+                lblMensajes.Visible = false;
+                txbCodigo.Text = "";
+                txbDescripcion.Text = "";
+
+                MessageBox.Show("Producto agregado correctamente.");
+            }
+            else
+            {
+                lblMensajes.Visible = true;
+                lblMensajes.Text = "Tiene que llenar ambos campos!";
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            //DialogResult res= MessageBox.Show(MessageBoxButtons.YesNo);
+            lsbInformacion.Items.Remove(lsbInformacion.SelectedItem);
+        }
+    }
+}
